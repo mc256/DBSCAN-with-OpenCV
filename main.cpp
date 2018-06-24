e0ec0fa688c71c8c35823c4ff9bf9c3f178a54d8
mc <null@yorku.ca>
2018-06-24 15:13:15 -0400

    random factor introduced

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
+#include <queue>
 #include "main.hpp"
 
 #define CLUSTER_UNDEFINED 0
 #define CLUSTER_NOISE -1
 #define RANGE_QUERY_FUNCTION circle_query_range
 
+struct Point_Unit{
+    Point2f point;
+    long index;
+    long order;
+    long label;
+
+    Point_Unit(int x, int y, long b){
+        point = Point2f(x,y);
+        index = b;
+        label = CLUSTER_UNDEFINED;
+    }
+};
+
 ///////////////////////////////////
 // Utility
 ///////////////////////////////////
 
-void load_data_set(vector<Point2f> * point_list, vector<long> * labels){
+void load_data_set(vector<Point_Unit *> * points){
     ifstream sample_file("/home/mc/Desktop/container/1/currenttime-11-32-36--224.png.data.txt");
     //ifstream sample_file("/home/mc/Desktop/container/2/currenttime-11-55-24--629.png.data.txt");
 
@@ -18,22 +32,22 @@ void load_data_set(vector<Point2f> * point_list, vector<long> * labels){
         if (buffer == "points") break;
     }
 
-    int x, y;
+    int x, y, i = 0;
     while (sample_file >> x >> y){
-        point_list->push_back(Point2f(x,y));
-        labels->push_back(CLUSTER_UNDEFINED);
+        points->emplace_back(new Point_Unit(x, y, i));
+        i++;
     }
 }
 
-Mat display_points_with_label(vector<Point2f> * point_list, vector<long> * labels){
+Mat display_points_with_label(vector<Point_Unit *> * point_list){
     int max_width, max_height;
     max_width = max_height = 0;
     for (const auto & item: *point_list){
-        if (item.x > max_width){
-            max_width = (int)item.x;
+        if (item->point.x > max_width){
+            max_width = (int)item->point.x;
         }
-        if (item.y > max_height){
-            max_height = (int)item.y;
+        if (item->point.y > max_height){
+            max_height = (int)item->point.y;
         }
     }
     //cout << max_height << "x" << max_width << endl;
@@ -42,20 +56,21 @@ Mat display_points_with_label(vector<Point2f> * point_list, vector<long> * label
     image = Scalar::all(0);
 
     set<int> label_set;
-    for (const auto & item: *labels){
-        label_set.insert(item);
+    for (const auto & item: *point_list){
+        label_set.insert(item->label);
     }
+    cout << "cluster: " << label_set.size() << endl;
 
     map<int,Scalar> random_color_list;
     for (const auto & item: label_set){
         random_color_list[item] = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
     }
 
-    for (auto i = 0; i < point_list->size(); ++i){
-        if (labels->at(i) == CLUSTER_NOISE){
-            circle(image, point_list->at(i), 1, random_color_list[labels->at(i)], 2);
+    for (long i = 0; i < point_list->size(); ++i){
+        if (point_list->at(i)->label == CLUSTER_NOISE){
+            circle(image, point_list->at(i)->point, 1, random_color_list[point_list->at(i)->label], 2);
         }else{
-            circle(image, point_list->at(i), 5, random_color_list[labels->at(i)], 2);
+            circle(image, point_list->at(i)->point, 5, random_color_list[point_list->at(i)->label], 2);
         }
 
 
@@ -70,24 +85,24 @@ Mat display_points_with_label(vector<Point2f> * point_list, vector<long> * label
 ///////////////////////////////////
 
 
-vector<unsigned long> square_query_range(vector<Point2f> * sorted_point_list, unsigned long mid, int range){
+vector<long> square_query_range(vector<Point_Unit *> * sorted_point, long mid, int range){
 
     // search left and right to see if the point falls in range
     //vector<Point2f> result;
-    vector<unsigned long> result;
-    auto i = mid;
-    while (i < sorted_point_list->size() && sorted_point_list->at(i).x < sorted_point_list->at(mid).x + range){
-        if ((sorted_point_list->at(i).y < sorted_point_list->at(mid).y + range) && (sorted_point_list->at(i).y > sorted_point_list->at(mid).y - range)){
-            //result.push_back(sorted_point_list->at(i));
-            result.push_back(i);
+    vector<long> result;
+    auto i = mid + 1;
+    while (i < sorted_point->size() && sorted_point->at(i)->point.x < sorted_point->at(mid)->point.x + range){
+        if ((sorted_point->at(i)->point.y < sorted_point->at(mid)->point.y + range) && (sorted_point->at(i)->point.y > sorted_point->at(mid)->point.y - range)){
+            //result.push_back(sorted_point->at(i));
+            result.push_back(sorted_point->at(i)->index);
         }
         i++;
     }
-    i = mid;
-    while (i > 0 && sorted_point_list->at(i).x > sorted_point_list->at(mid).x - range){
-        if ((sorted_point_list->at(i).y < sorted_point_list->at(mid).y + range) && (sorted_point_list->at(i).y > sorted_point_list->at(mid).y - range)){
-            //result.push_back(sorted_point_list->at(i));
-            result.push_back(i);
+    i = mid - 1;
+    while (i >= 0 && sorted_point->at(i)->point.x > sorted_point->at(mid)->point.x - range){
+        if ((sorted_point->at(i)->point.y < sorted_point->at(mid)->point.y + range) && (sorted_point->at(i)->point.y > sorted_point->at(mid)->point.y - range)){
+            //result.push_back(sorted_point->at(i));
+            result.push_back(sorted_point->at(i)->index);
         }
         i--;
     }
@@ -96,25 +111,25 @@ vector<unsigned long> square_query_range(vector<Point2f> * sorted_point_list, un
     return result;
 }
 
-vector<unsigned long> circle_query_range(vector<Point2f> * sorted_point_list, unsigned long mid, int range) {
+queue<long> circle_query_range(vector<Point_Unit *> * sorted_point, long mid, int range) {
 
-    int range_squred = range * range;
-    vector<unsigned long> result;
-    auto i = mid;
-    while (i < sorted_point_list->size() && sorted_point_list->at(i).x < sorted_point_list->at(mid).x + range){
-        auto delta_x = (sorted_point_list->at(i).x-sorted_point_list->at(mid).x);
-        auto delta_y = (sorted_point_list->at(i).y-sorted_point_list->at(mid).y);
-        if (delta_x * delta_x + delta_y * delta_y < range_squred){
-            result.push_back(i);
+    int range_squared = range * range;
+    queue<long> result;
+    long i = mid + 1;
+    while (i < sorted_point->size() && sorted_point->at(i)->point.x < sorted_point->at(mid)->point.x + range){
+        auto delta_x = (sorted_point->at(i)->point.x-sorted_point->at(mid)->point.x);
+        auto delta_y = (sorted_point->at(i)->point.y-sorted_point->at(mid)->point.y);
+        if (delta_x * delta_x + delta_y * delta_y < range_squared){
+            result.push(sorted_point->at(i)->index);
         }
         i++;
     }
-    i = mid;
-    while (i > 0 && sorted_point_list->at(i).x > sorted_point_list->at(mid).x - range){
-        auto delta_x = (sorted_point_list->at(i).x-sorted_point_list->at(mid).x);
-        auto delta_y = (sorted_point_list->at(i).y-sorted_point_list->at(mid).y);
-        if (delta_x * delta_x + delta_y * delta_y < range_squred){
-            result.push_back(i);
+    i = mid - 1;
+    while (i >= 0 && sorted_point->at(i)->point.x > sorted_point->at(mid)->point.x - range){
+        auto delta_x = (sorted_point->at(i)->point.x-sorted_point->at(mid)->point.x);
+        auto delta_y = (sorted_point->at(i)->point.y-sorted_point->at(mid)->point.y);
+        if (delta_x * delta_x + delta_y * delta_y < range_squared){
+            result.push(sorted_point->at(i)->index);
         }
         i--;
     }
@@ -123,23 +138,23 @@ vector<unsigned long> circle_query_range(vector<Point2f> * sorted_point_list, un
     return result;
 }
 
-vector<unsigned long> square_query_range(vector<Point2f> * sorted_point_list, Point2f point, int range) {
+vector<long> square_query_range(vector<Point_Unit *> * sorted_point, Point2f point, int range) {
     // binary search point
-    unsigned long left, mid, right;
+    long left, mid, right;
     left = 0;
-    right = sorted_point_list->size() -1;
+    right = sorted_point->size() -1;
     mid = (left+right)/2;
     while (left < mid){
-        if (sorted_point_list->at(mid).x < point.x){
+        if (sorted_point->at(mid)->point.x < point.x){
             left = mid;
-        }else if (sorted_point_list->at(mid).x > point.x){
+        }else if (sorted_point->at(mid)->point.x > point.x){
             right = mid;
         }else{
             break;
         }
         mid = (left + right)/2;
     }
-    square_query_range(sorted_point_list, mid, range);
+    square_query_range(sorted_point, mid, range);
 }
 
 
@@ -148,35 +163,39 @@ vector<unsigned long> square_query_range(vector<Point2f> * sorted_point_list, Po
 // Algorithm
 ///////////////////////////////////
 
-void density_based_spatial_clustering_of_application_with_noise(vector<Point2f> * sorted_point_list, vector<long> * labels, int range, int density){
-    for (auto & item : *labels){
-        item = CLUSTER_UNDEFINED;
+void density_based_spatial_clustering_of_application_with_noise(vector<Point_Unit *> * unsorted_points, vector<Point_Unit *> * sorted_point, int range, int density){
+    for (auto & item : *unsorted_points){
+        item->label = CLUSTER_UNDEFINED;
     }
-    long cluster = 0;
-    for (unsigned long i = 0; i < sorted_point_list->size(); ++i){
-        if (labels->at(i) != CLUSTER_UNDEFINED) {
+    int cluster = 0;
+    for (unsigned long i = 0; i < unsorted_points->size(); ++i){
+        if (unsorted_points->at(i)->label != CLUSTER_UNDEFINED) {
             continue;
         }
-        auto neighbours = RANGE_QUERY_FUNCTION(sorted_point_list, i, range);
+        auto neighbours = RANGE_QUERY_FUNCTION(sorted_point, unsorted_points->at(i)->order, range);
         if (neighbours.size() < density){
-            labels->at(i) = CLUSTER_NOISE;
+            unsorted_points->at(i)->label = CLUSTER_NOISE;
             continue;
         }
         cluster ++;
-        labels->at(i) = cluster;
-        for (const auto & item: neighbours){
-            if (labels->at(item) != CLUSTER_NOISE){
-                labels->at(item) = cluster;
+        unsorted_points->at(i)->label = cluster;
+        while (!neighbours.empty()){
+            long item = neighbours.front();
+            neighbours.pop();
+
+            if (unsorted_points->at(item)->label == CLUSTER_NOISE){
+                unsorted_points->at(item)->label = cluster;
                 continue;
             }
-            if (labels->at(item) != CLUSTER_UNDEFINED){
+            if (unsorted_points->at(item)->label != CLUSTER_UNDEFINED){
                 continue;
             }
-            labels->at(item) = cluster;
-            auto recursive_neighbours = RANGE_QUERY_FUNCTION(sorted_point_list, item, range);
-            if (recursive_neighbours.size() > density){
-                for (const auto & k : recursive_neighbours){
-                    neighbours.push_back(k);
+            unsorted_points->at(item)->label = cluster;
+            auto recursive_neighbours = RANGE_QUERY_FUNCTION(sorted_point, unsorted_points->at(item)->order, range);
+            if (recursive_neighbours.size() >= density){
+                while (!recursive_neighbours.empty()){
+                    neighbours.push(recursive_neighbours.front());
+                    recursive_neighbours.pop();
                 }
             }
         }
@@ -185,29 +204,36 @@ void density_based_spatial_clustering_of_application_with_noise(vector<Point2f> 
 
 }
 
+void sort_points(vector<Point_Unit *> * unsorted_points, vector<Point_Unit *> * sorted_points){
+    // Sort Points
+    copy(unsorted_points->begin(), unsorted_points->end(), back_inserter(*sorted_points));
+    sort(sorted_points->begin(), sorted_points->end(), [](Point_Unit * a, Point_Unit * b)->bool{
+        return a->point.x < b->point.x;
+    });
+    int count = 0;
+    for (const auto & item: *sorted_points){
+        item->order = count ++;
+        //cout << "(" << item->point.x << "," << item->point.y << ") "  << item->index << " - "  << item->order << endl;
+    }
+
+}
+
 int src_range, src_density;
 
-vector<Point2f> points;
-vector<long> labels;
+vector<Point_Unit *> points, sorted_points;
 
 Mat display_image;
 
 void update_image(int a, void * b){
-    density_based_spatial_clustering_of_application_with_noise(&points, &labels, src_range, src_density);
-    display_image = display_points_with_label(&points, &labels);
+    density_based_spatial_clustering_of_application_with_noise(&points, &sorted_points, src_range, src_density);
+    display_image = display_points_with_label(&points);
     imshow("test", display_image);
 }
 
 int main() {
 
-    load_data_set(&points, &labels);
-    sort(points.begin(), points.end(), [](Point2f a, Point2f b)->bool{
-        return a.x < b.x;
-    });
-
-    for (const auto & item: points){
-        cout << "(" << item.x << "," << item.y << ")" << endl;
-    }
+    load_data_set(&points);
+    sort_points(&points, &sorted_points);
 
     src_range = 120;
     src_density = 30;
@@ -215,30 +241,13 @@ int main() {
     // Configuration
     namedWindow("test", WINDOW_FREERATIO);
 
-    /*
-    unsigned long target = 150;
-    int range = 100;
-    auto point_set = RANGE_QUERY_FUNCTION(&points, target, range);
-    for (const auto & item: point_set){
-        labels.at(item) = 2223;
-    }
-    cout << point_set.size() << endl;
-    labels.at(target) = 10;
-    display_image = display_points_with_label(&points, &labels);
-    circle(display_image, points.at(target),range,Scalar(128,128,255),3);
-    imshow("test", display_image);
-    waitKey(0);
-    return 0;
-    */
-
     // Display
     for(;;) {
         createTrackbar("src_range", "test", &src_range, 300, update_image);
         createTrackbar("src_density", "test", &src_density, 300, update_image);
 
-        density_based_spatial_clustering_of_application_with_noise(&points, &labels, src_range, src_density);
-        display_image = display_points_with_label(&points, &labels);
-        imshow("test", display_image);
+        update_image(0, nullptr);
+
         if (waitKey(0) == 'q') break;
     }
 
